7f7e45d700a1bcfaf5e0c4393e96b84941bd9fe6
Leonardo Salazar <salazarlj@gmail.com>
2022-01-22 14:57:50 -0600

    I repaired a small problem on loop for

--- title.py
+++ title.py
@@ -1,11 +1,11 @@
 def main():
 
-    for i in range (1,4):
+    for i in range (1,3):
         print(f"LINE # {i} Welcome")
 
-        for j in range (1,6):
+        for j in range (1,4):
             print (f" SUBTOPIC # {i} .{j}")
         print ("\n")
-
+ 
 if __name__ =="__main__":
     main()
